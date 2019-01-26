577b9b181e209325d0f1c9e93f471a6e55629436
Suresh Chandak <sureshchandak1@gmail.com>
2019-01-27 01:36:44 +0530

    Design Patterns -> Decorator Pattern example

---
+++ src/designPatterns/structuralDesignPatterns/decoratorPattern/Food.java
@@ -0,0 +1,9 @@
+package designPatterns.structuralDesignPatterns.decoratorPattern;
+
+public interface Food {
+
+	public String prepareFood();
+
+	public double foodPrice();
+
+}
---
+++ src/designPatterns/structuralDesignPatterns/decoratorPattern/VegFood.java
@@ -0,0 +1,15 @@
+package designPatterns.structuralDesignPatterns.decoratorPattern;
+
+public class VegFood implements Food {
+
+	@Override
+	public String prepareFood() {
+		return "Veg Food";
+	}
+
+	@Override
+	public double foodPrice() {
+		return 50.0;  
+	}
+
+}
