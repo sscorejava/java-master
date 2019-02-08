07355fd942ef43971181c1bb0d57735cb37c9916
Suresh Chandak <sureshchandak1@gmail.com>
2019-02-08 17:16:52 +0530

    Chain Of Responsibility Pattern example

---
+++ src/designPatterns/behavioralDesignPatterns/chainOfResponsibility/ConsoleBasedLogger.java
@@ -0,0 +1,14 @@
+package designPatterns.behavioralDesignPatterns.chainOfResponsibility;
+
+public class ConsoleBasedLogger extends Logger {
+	
+	public ConsoleBasedLogger(int levels) {
+		this.levels = levels;
+	}
+
+	@Override
+	protected void displayLogInfo(String msg) {
+		System.out.println("CONSOLE LOGGER INFO: " + msg);
+	}
+	
+}
---
+++ src/designPatterns/behavioralDesignPatterns/chainOfResponsibility/ErrorBasedLogger.java
@@ -0,0 +1,14 @@
+package designPatterns.behavioralDesignPatterns.chainOfResponsibility;
+
+public class ErrorBasedLogger extends Logger {
+	
+	public ErrorBasedLogger(int levels) {
+		this.levels = levels;
+	}
+
+	@Override
+	protected void displayLogInfo(String msg) {
+		System.out.println("ERROR LOGGER INFO: " + msg);
+	}
+	
+}
